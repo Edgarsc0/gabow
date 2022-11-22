5f2d815e661c93cdac4299ec458a20c4e1665543
bviverosa <bviverosa2100@alumno.ipn.mx>
2022-11-22 15:31:42 -0600

    comillas

--- pages/Datos.jsx
+++ pages/Datos.jsx
@@ -1,4 +1,4 @@
-import axios from 'axios'
+// import axios from 'axios'
 import Head from 'next/head'
 import React from 'react'
 import styles from '../styles/Datos.module.scss'
@@ -18,15 +18,15 @@ const Datos = () => {
                 <table className={styles.tabla}>
                     <tbody>
                         <tr>
-                            <td className={styles.celda}>Nombre: "Nombre del usuario"</td>
+                            <td className={styles.celda}>Nombre: Nombre del usuario</td>
                             <td className={styles.celda}><button className={styles.button}>Editar</button></td>
                         </tr>
                         <tr>
-                            <td className={styles.celda}>Correo: "Correo del usuario"</td>
+                            <td className={styles.celda}>Correo: Correo del usuario</td>
                             <td className={styles.celda}><button className={styles.button}>Editar</button></td>
                         </tr>
                         <tr>
-                            <td className={styles.celda}>Contraseña: "Contraseña del usuario"</td>
+                            <td className={styles.celda}>Contraseña: Contraseña del usuario</td>
                             <td className={styles.celda}><button className={styles.button}>Editar</button></td>
                         </tr>
                         <tr>
