b8d5927fb51a6d5261ff7883a4290a6faa57e102
Edgarsc0 <esanchezc2004@alumno.ipn.mx>
2023-04-18 15:27:49 -0600

    nueva act

--- components/Info.jsx
+++ components/Info.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Drawer, Box, Typography, IconButton, Grid, Snackbar, useMediaQuery } from "@mui/material"
 import MenuIcon from '@mui/icons-material/Menu';
 import styles from '../styles/Info.module.scss'
+import Link from "next/link";
 
 const Info = ({ openInfo, closeInfo, room }) => {
 
@@ -26,17 +27,28 @@ const Info = ({ openInfo, closeInfo, room }) => {
                             <MenuIcon className={styles.icon} />
                         </IconButton>
                     </Grid>
-                    <Grid item>
-                        <div className={styles.cont}>
-                            <h2 className={styles.logo}>GABOW</h2>
-                        </div>
-                    </Grid>
                 </Grid>
             </Snackbar>
             <Drawer anchor={isDesktop ? 'left' : 'bottom'} open={isOpen} onClose={closeAll}>
                 <Box className={styles.box}>
                     <Typography variant='h6' role='presentation'>
-                        Mensaje de prueba del cuarto {room}
+                        <h2>Gabow</h2>
+                        <hr></hr>
+                    </Typography>
+                </Box>
+                <Box className={styles.box}>
+                    <Typography variant='' role='presentation'>
+                        <Link href="/"><p>Home</p></Link>
+                    </Typography>
+                </Box>
+                <Box className={styles.box}>
+                    <Typography variant='' role='presentation'>
+                        <Link href="https://edgarsc0.github.io/prueba-grafos/"><p>Rutas</p></Link>
+                    </Typography>
+                </Box>
+                <Box className={styles.box}>
+                    <Typography variant='' role='presentation'>
+                        <Link href="https://edgarsc0.github.io/prueba-gabow-InsideView/"><p>Gabow InsideView Demo</p></Link>
                     </Typography>
                 </Box>
             </Drawer>
